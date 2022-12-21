107d9bb2a3ebb394ba0a676e0ab58ce9e3f48741
BSEF19M513 <bsef19m513@pucit.edu.pk>
2022-12-21 22:47:24 +0500

    Admin Page UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { Admin } from "./components/Admin";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   
   return (
     // main container
-    <div className=" bg-[#F7F7F7] main_container  w-screen h-screen flex flex-col items-center justify-center ">
+    <div className=" bg-[#F7F7F7] main_container  w-screen h-screen">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </div>      
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,7 +18,6 @@ function Login()
         }
     }
 
-    // useHistory hook for redirecting to another page
     const history = useNavigate();
 
     return (
@@ -31,14 +30,16 @@ function Login()
                 <p className='relative bottom-4 text-slate-600 ' >Access to our dashboard</p>
                 <div className="form w-full h-1/2 flex flex-col items-center justify-center">
                     <label htmlFor="email" className='absolute top-[300px] left-[460px] mb-10 ' >Email Address</label>
-                    <input type="email" placeholder="username123@gmail.com" className=" outline-none  w-[90%] h-[40px] border-2 border-gray-300 rounded-md mt-4 mb-10 p-4 " />
+                    <input type="email" placeholder="username123@gmail.com" className=" outline-none  w-[90%] h-[40px] border-2 border-gray-300 rounded-md mt-4 mb-10 p-4 " required />
                     <label htmlFor="password" className='absolute top-[362px] mt-4 left-[460px]' >Password</label>
-                    <input type={password} placeholder="Password" className="password p-4 outline-none w-[90%] h-[40px] border-2 border-gray-300 rounded-md"
+                    <input type={password} placeholder="Password" className="password p-4 outline-none w-[90%] h-[40px] border-2 border-gray-300 rounded-md" required
                     />
 
                     <span className=" password_span cursor-pointer absolute top-[415px] right-[470px] text-slate-600 " onClick={() => showPassword()}><i className={`fas ${passIcon}`}></i></span>
 
-                    <button className="h-1/4 text-lg font-semibold bg-blue-500 text-white rounded-md w-[90%] relative top-8 ">Login</button>
+                    <button className="h-1/4 text-lg font-semibold bg-blue-500 text-white rounded-md w-[90%] relative top-8 "
+                        onClick={() => history('/admin')}
+                    >Login</button>
 
                     {/* don't have an account */}
                     <div className="relative top-10 text-md text-slate-600 ">
