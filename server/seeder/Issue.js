d3e74697199d0f1d97055ca3d07309b21b2bc1a4
devsalman247 <salman247.dev@gmail.com>
2023-01-01 20:59:21 +0500

    items seeder fixes

--- server/seeder/Issue.js
+++ server/seeder/Issue.js
@@ -5,25 +5,110 @@ async function seedIssuedItems() {
 	// count the number of items in issuedItem collection of database
 	const count = await Issue.countDocuments();
 
-	const issuedItem1 = new Issue({
-		issuedId: count,
-		issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
-		items: [
-			{
-				id: "60f2b7f0d6e1a92f1c8b9c9a",
-				quantity: 2,
-				returned: [
-					{
-						quantity: 1,
-						returnDate: Date.now(),
-					},
-				],
-			},
-		],
-		issueDate: Date.now(),
-		lastReturn: Date.now(),
+	Item.find({}).then((err, items) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(items);
+		}
 	});
-	await issue.save();
+
+	// const issuedItem1 = new Issue({
+	// 	issuedId: count + 1,
+	// 	issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 	items: [
+	// 		{
+	// 			id: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 			quantity: 2,
+	// 			returned: [
+	// 				{
+	// 					quantity: 1,
+	// 					returnDate: Date.now(),
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// 	issueDate: Date.now(),
+	// 	lastReturn: Date.now(),
+	// });
+
+	// const issuedItem2 = new Issue({
+	// 	issuedId: count + 2,
+	// 	issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 	items: [
+	// 		{
+	// 			id: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 			quantity: 2,
+	// 			returned: [
+	// 				{
+	// 					quantity: 1,
+	// 					returnDate: Date.now(),
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// 	issueDate: Date.now(),
+	// 	lastReturn: Date.now(),
+	// });
+
+	// const issuedItem3 = new Issue({
+	// 	issuedId: count + 3,
+	// 	issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 	items: [
+	// 		{
+	// 			id: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 			quantity: 2,
+	// 			returned: [
+	// 				{
+	// 					quantity: 1,
+	// 					returnDate: Date.now(),
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// 	issueDate: Date.now(),
+	// 	lastReturn: Date.now(),
+	// });
+
+	// const issuedItem4 = new Issue({
+	// 	issuedId: count + 4,
+	// 	issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 	items: [
+	// 		{
+	// 			id: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 			quantity: 2,
+	// 			returned: [
+	// 				{
+	// 					quantity: 1,
+	// 					returnDate: Date.now(),
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// 	issueDate: Date.now(),
+	// 	lastReturn: Date.now(),
+	// });
+
+	// const issuedItem5 = new Issue({
+	// 	issuedId: count + 5,
+	// 	issuedTo: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 	items: [
+	// 		{
+	// 			id: "60f2b7f0d6e1a92f1c8b9c9a",
+	// 			quantity: 2,
+	// 			returned: [
+	// 				{
+	// 					quantity: 1,
+	// 					returnDate: Date.now(),
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// 	issueDate: Date.now(),
+	// 	lastReturn: Date.now(),
+	// });
+
+	// await issuedItem1.save();
 }
 
 export default seedIssuedItems;
