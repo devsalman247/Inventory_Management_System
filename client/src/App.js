5b0971988c0a70b55635c1501e9976d25d69544f
devsalman247 <salman247.dev@gmail.com>
2023-01-14 00:18:47 +0500

    some login fixes

--- client/src/App.js
+++ client/src/App.js
@@ -11,24 +11,6 @@ function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const token = JSON.parse(localStorage.getItem("user"))?.token;
 
-	useEffect(() => {
-		if (token) {
-			axios
-				.get("http://localhost:5000/user/authenticate", {
-					headers: { Authorization: `Token ${token}` },
-				})
-				.then((res) => {
-					if (res.status === 200) {
-						setIsLoggedIn(true);
-						navigateTo("/admin");
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	}, []);
-
 	return (
 		// main container
 		<div className=" bg-[#F7F7F7] main_container  w-screen h-screen">
