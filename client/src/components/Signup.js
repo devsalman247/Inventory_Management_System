38b8801e30ef85da5f8df52ec9d3ab595a9731fb
devsalman247 <salman247.dev@gmail.com>
2023-01-14 00:31:01 +0500

    added signup api

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,18 @@
 import logo from "../logo.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function Signup() {
 	const [password, setPassword] = useState("password");
 	const [passIcon, setPassIcon] = useState("fa-eye-slash");
+	const [passwordValue, setPasswordValue] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [email, setEmail] = useState("");
+
+	// useHistory hook for redirecting to another page
+	const history = useNavigate();
 
 	const showPassword = () => {
 		if (password === "password") {
@@ -16,8 +24,51 @@ function Signup() {
 		}
 	};
 
-	// useHistory hook for redirecting to another page
-	const history = useNavigate();
+	const showMessage = (message, type) => {
+		Swal.fire({
+			toast: true,
+			icon: type,
+			title: message,
+			animation: false,
+			position: "bottom",
+			showConfirmButton: false,
+			timer: 3000,
+			timerProgressBar: true,
+			didOpen: (toast) => {
+				toast.addEventListener("mouseenter", Swal.stopTimer);
+				toast.addEventListener("mouseleave", Swal.resumeTimer);
+			},
+		});
+	};
+
+	const handleSignUp = () => {
+		console.log(passwordValue, confirmPassword);
+		if (!email) {
+			showMessage("Email field cannot be empty!", "error");
+			return;
+		} else if (!passwordValue) {
+			showMessage("Password field cannot be empty!", "error");
+			return;
+		} else if (!confirmPassword) {
+			showMessage("Confirm Password field cannot be empty!", "error");
+			return;
+		} else {
+			axios
+				.post("http://localhost:5000/user/signup", { email, password })
+				.then((res) => {
+					if (res.status === 200) {
+						showMessage("You've successfully registered!", "success");
+						return history("/admin");
+					} else {
+						showMessage("Signup failed...!", "error");
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+					showMessage(err.response.data.message, "error");
+				});
+		}
+	};
 
 	return (
 		// main container
@@ -33,6 +84,8 @@ function Signup() {
 					</label>
 					<input
 						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
 						placeholder="username123@gmail.com"
 						className=" outline-none  w-[90%] h-[40px] border-2 border-gray-300 rounded-md mt-4 p-4"
 						required
@@ -44,6 +97,8 @@ function Signup() {
 					</label>
 					<input
 						type={password}
+						value={passwordValue}
+						onChange={(e) => setPasswordValue(e.target.value)}
 						placeholder="Password"
 						className="password p-4 outline-none w-[90%] h-[40px] border-2 border-gray-300 rounded-md "
 						required
@@ -61,12 +116,16 @@ function Signup() {
 					</label>
 					<input
 						type={password}
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
 						placeholder="Password"
 						className="password p-4 outline-none w-[90%] h-[40px] border-2 border-gray-300 rounded-md"
 						required
 					/>
 
-					<button className=" py-3 text-lg font-semibold bg-blue-500 text-white rounded-md w-[90%] relative top-2">
+					<button
+						onClick={() => handleSignUp()}
+						className=" py-3 text-lg font-semibold bg-blue-500 text-white rounded-md w-[90%] relative top-2">
 						Register
 					</button>
 
