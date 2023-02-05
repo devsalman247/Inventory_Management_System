ca28007e35a5d5fa36d494e212a234573ad65c95
devsalman247 <salman247.dev@gmail.com>
2023-02-05 19:54:23 +0500

    some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,78 @@
 import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { Admin } from "./components/Admin";
-import { AddEmployee } from "./components/AddEmployee";
-import { UpdateEmployee } from "./components/UpdateEmployee";
-import { DeleteEmployee } from "./components/DeleteEmployee";
-import { AddItem } from "./components/AddItem";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Admin } from "./components/Admin/Admin";
+import { AddEmployee } from "./components/Admin/AddEmployee";
+import { UpdateEmployee } from "./components/Admin/UpdateEmployee";
+import { DeleteEmployee } from "./components/Admin/DeleteEmployee";
+import { AddItem } from "./components/Admin/AddItem";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AuthContext } from "./context_store";
 import axios from "axios";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const token = JSON.parse(localStorage.getItem("user"))?.token;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const token = JSON.parse(localStorage.getItem("user"))?.token;
 
-	useEffect(() => {
-		(function () {
-			const token = JSON.parse(localStorage.getItem("user"))?.token;
-			if (token) {
-				axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-			} else {
-				axios.defaults.headers.common["Authorization"] = null;
-				/*if setting null does not remove `Authorization` header then try     
+  useEffect(() => {
+    (function () {
+      const token = JSON.parse(localStorage.getItem("user"))?.token;
+      if (token) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      } else {
+        axios.defaults.headers.common["Authorization"] = null;
+        /*if setting null does not remove `Authorization` header then try     
 				delete axios.defaults.headers.common['Authorization'];
 				*/
-			}
-		})();
-	}, []);
+      }
+    })();
+  }, []);
 
-	return (
-		// main container
-		<div className=" bg-[#F7F7F7] main_container  w-screen h-screen">
-			<AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-				<Router>
-					<Routes>
-						<Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} token={token} />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route
-							path="/admin"
-							element={isLoggedIn ? <Admin setIsLoggedIn={setIsLoggedIn} token={token} /> : <Navigate to={"/"} />}
-						/>
-						<Route path="/add_employee" element={<AddEmployee />} />
-						<Route path="/update_employee" element={<UpdateEmployee />} />
-						<Route path="/delete_employee" element={<DeleteEmployee />} />
-						<Route path="/add_item" element={<AddItem />} />
-					</Routes>
-				</Router>
-			</AuthContext.Provider>
-		</div>
-	);
+  return (
+    // main container
+    <div className=" bg-[#F7F7F7] main_container  w-screen h-screen">
+      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<Login setIsLoggedIn={setIsLoggedIn} token={token} />}
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/admin"
+              element={
+                isLoggedIn ? (
+                  <Admin setIsLoggedIn={setIsLoggedIn} token={token} />
+                ) : (
+                  <Navigate to={"/"} />
+                )
+              }
+            />
+            <Route
+              path="/user"
+              element={
+                isLoggedIn ? (
+                  <Admin setIsLoggedIn={setIsLoggedIn} token={token} />
+                ) : (
+                  <Navigate to={"/"} />
+                )
+              }
+            />
+            <Route path="/add_employee" element={<AddEmployee />} />
+            <Route path="/update_employee" element={<UpdateEmployee />} />
+            <Route path="/delete_employee" element={<DeleteEmployee />} />
+            <Route path="/add_item" element={<AddItem />} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- server/seed.js
+++ server/seed.js
@@ -6,25 +6,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose
-	.connect(`${process.env.MONGO_URI_LOCAL}`)
-	.then(() => {
-		console.log("connected to db in development environment");
-		init();
-	})
-	.catch((err) => {
-		console.log(err);
-		process.exit(1);
-	});
+  .connect(`${process.env.MONGO_URI_LOCAL}`)
+  .then(() => {
+    console.log("connected to db in development environment");
+    init();
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
 async function init() {
-	console.log("dropping DB");
-	await mongoose.connection.db.dropDatabase();
-	await userSeeder();
-	await itemSeeder();
-	await issueSeeder();
-	exit();
+  console.log("dropping DB");
+  await mongoose.connection.db.dropDatabase();
+  await userSeeder();
+  await itemSeeder();
+  await issueSeeder();
+  exit();
 }
 function exit() {
-	console.log("exiting");
-	process.exit(1);
+  console.log("exiting");
+  process.exit(1);
 }
